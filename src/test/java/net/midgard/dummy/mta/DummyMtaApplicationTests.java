a86755e84ce5824856cd61e7264233bf89c5e9c1
Erik Johnson <johnson.erik.n@gmail.com>
2022-02-26 17:44:50 -0500

    Adding test automation

--- src/test/java/net/midgard/dummy/mta/DummyMtaApplicationTests.java
+++ src/test/java/net/midgard/dummy/mta/DummyMtaApplicationTests.java
@@ -1,13 +1,41 @@
 package net.midgard.dummy.mta;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class DummyMtaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void getEmailCount() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/email/count").accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk()).andExpect(content().string(equalTo("0")));
+    }
+
+    @Test
+    void getEmail() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/email/1").accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void searchEmail() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/email?searchTerm=term").accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isNotFound());
+    }
 
 }
